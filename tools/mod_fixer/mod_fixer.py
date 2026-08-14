1ffb89ae5446bb9ca271341e58bf929fbbe0e563
agungmubarak1453 <agungmubarak1453@gmail.com>
2026-08-14 19:43:49 +0700

    feat: add other two for experimental fix code

--- tools/mod_fixer/mod_fixer.py
+++ tools/mod_fixer/mod_fixer.py
@@ -50,4 +50,6 @@ def fix(mod_path=".", fix_code=None, is_dry_run=False):
 
         match fix_code:
             case "experimental":
+                get_script("hash_fixer.py")["fix"](mod_path, is_dry_run)
+                get_script("rabbitfx_fixer.py")["fix"](mod_path, is_dry_run)
                 get_script("experimental_hash_fixer.py")["fix"](mod_path, is_dry_run)
